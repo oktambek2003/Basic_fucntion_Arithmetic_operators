8302e0f1de91bee4c64723421823f9f051d31669
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-10-26 19:27:55 +0500

    3-misol

--- bin/arithmetic03.dart
+++ bin/arithmetic03.dart
@@ -1,5 +1,11 @@
 // Create a function called func.
 // CCreate function arguments a and b of type int.
 // Returns the remainder of a and b.
+int func(int a, int b) {
+  int d = a % b;
+  return d;
+}
 
-void main() {}
+void main() {
+  print(func(5, 3));
+}
