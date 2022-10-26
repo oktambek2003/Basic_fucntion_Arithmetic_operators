fee349abad02d4507aee2b72e01291864c9698f3
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-10-26 19:30:00 +0500

    1-misol

--- bin/arithmetic01.dart
+++ bin/arithmetic01.dart
@@ -3,6 +3,11 @@
 // Create a parameter int type y of type int.
 // return sum of two parameters.
 
-void main() {
+int add(int x,int y) {
+  int d = x + y;
+  return d;
+}
 
+void main() {
+  print(add(12,23));
 }
