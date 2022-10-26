64a1a62333e2c5b80dae5c2b661ae6bdf112684b
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-10-26 19:50:32 +0500

    6misol

--- bin/arithmetic06.dart
+++ bin/arithmetic06.dart
@@ -3,5 +3,13 @@
 // the two-digit numbers will be given.
 // Find the reverse of the number and return to a variable called ‘answer’
 // return answer
+int func(int a) {
+  int b = a % 10;
+  int d = a ~/ 10;
+  int c = d + b*10;
+  return c;
+}
 
-void main() {}
+void main() {
+  print(func(23));
+}
