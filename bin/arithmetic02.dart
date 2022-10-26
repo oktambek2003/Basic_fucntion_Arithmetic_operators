fb568058fa0c47578c7bd543b58d59df22de37e1
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-10-26 19:29:42 +0500

    2-misol

--- bin/arithmetic02.dart
+++ bin/arithmetic02.dart
@@ -1,7 +1,11 @@
 // Create a function called divide.
 // Create function arguments a and b of type int.
 // Returns the floor division of a and b.
+int divide(int a, int b) {
+  int d = a % b;
+  return d;
+}
 
 void main() {
-  
+  print(divide(4, 2));
 }
