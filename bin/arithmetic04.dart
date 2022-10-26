ee659d8df10bd6f3abd845a09b511ff208dc05bb
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-10-26 19:40:12 +0500

    4-misol

--- bin/arithmetic04.dart
+++ bin/arithmetic04.dart
@@ -1,5 +1,11 @@
 // Create a function called multiple.
 // Create function arguments a and b of type int.
 // Return the multiplication of a and b.
+int multiple(int a, int b) {
+  int d = a * b;
+  return d;
+}
 
-void main() {}
+void main() {
+  print(multiple(5, 3));
+}
