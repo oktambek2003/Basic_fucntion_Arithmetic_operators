b630a6afbd9f3a2ae80c7dd201dcd7508bc30885
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-10-26 19:44:43 +0500

    5-misol

--- bin/arithmetic05.dart
+++ bin/arithmetic05.dart
@@ -1,6 +1,15 @@
-// Create a function called func.
+//Create a function called func.
 // Create a fucntion argument ‘number’
 // two digits numbers will be given.
 // return the sum of the number’s digits.
+//
+int func(int a) {
+  int b = a % 10;
+  int d = a ~/ 10;
+  int c = b + d;
+  return c;
+}
 
-void main() {}
+void main() {
+  print(func(23));
+}
