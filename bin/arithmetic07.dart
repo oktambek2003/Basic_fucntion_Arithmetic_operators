3e9c20ee7a82fb2c5d201eca2d86f9a9b5cfbb88
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-10-27 02:33:23 +0500

    7-misol

--- bin/arithmetic07.dart
+++ bin/arithmetic07.dart
@@ -1,10 +1,18 @@
 // Create a function called func.
-// Create a function argument  called ‘number’ of type int 
+// Create a function argument  called ‘number’ of type int
 // the three-digit number will be given.
 // Find the ‘number’ first digit and assign to x1.
 // Find the ‘number’ second digit and assign to x2.
 // Find the ‘number’ third digit and assign to x3.
 // Create a variable called ‘answer’ and assign it the sum of the three digits.
 // return the sum of the digits.
+int func(int a) {
+  int x1 = a % 10;
+  int x2 = (a % 100) ~/ 10;
+  int x3 = a ~/ 100;
+  return x1 + x2 + x3;
+}
 
-void main() {}
+void main() {
+  print(func(456));
+}
